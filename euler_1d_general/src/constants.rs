2f8638124a39ee34fcb78bb647901966564a398f
Sailor Koeplinger <40185227+MrPezser@users.noreply.github.com>
2026-02-12 18:39:49 -0500

    starting to work on solution type

--- euler_1d_general/src/constants.rs
+++ euler_1d_general/src/constants.rs
@@ -6,3 +6,7 @@ pub const N_CELLS: usize = 400;      // Number of spatial grid points
 pub const L_DOMAIN: f64 = 1.0;       // Length of the domain (meters)
 pub const T_FINAL: f64 = 0.2;        // Final simulation time (seconds)
 pub const CFL: f64 = 0.5;            // Courant-Friedrichs-Lewy stability number
+//
+pub const AS_IMM: i32 = 0;
+pub const AS_MUT: i32 = 1;
+pub const AS_OWN: i32 = 2;
